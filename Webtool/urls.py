083c45dd2b039a5aa3c39375c843cda0f97f95fc
Anirudh <anen08saks@gmail.com>
2025-05-03 03:44:59 +0530

    Multiple changes such as added html files to templates , added a static folder , changed views.py settings.py and urls.py , for some reason the css wont show up and I'll ask someone how to fix it tommorow

--- Webtool/urls.py
+++ Webtool/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.contrib.auth.views import LogoutView  # 👈 import this
+from django.contrib.auth.views import LogoutView  
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
     path("auth/", include('social_django.urls', namespace="social")),
-    path('logout/', LogoutView.as_view(), name='logout'),  # 👈 add this
-    path('', RedirectView.as_view(url='/authentication/')),  # 👈 Add this line
-]
+    path('logout/', LogoutView.as_view(), name='logout'),  
+    path('', RedirectView.as_view(url='/authentication/')),  
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
